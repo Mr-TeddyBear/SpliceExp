Completed ma plot aleted ma plot and fixed an overflow issue in normalization

src/help_functions/median_of_ratios_normalization.py
@@ -16,12 +16,11 @@ def median_of_ratios_normalization(exon_junction_df, samples):
     """
     ratio_df = exon_junction_df.copy()
     # Calcualte the psedua referance sample value
-    ratio_df["pseudo_reference_sample"] = exon_junction_df[samples].apply(lambda x: np.power(x[x != 0].prod(), 1/len(x[x != 0])), axis=1)
-
+    ratio_df["pseudo_reference_sample"] = exon_junction_df[samples].apply(lambda x: np.power(x[x != 0], 1/len(x[x != 0])).prod(), axis=1)
     #display(ratio_df)
 
     #Calcualte normalisation facotr for rows
-    ratio_df[samples] =ratio_df.apply(lambda x: x[samples]/x["pseudo_reference_sample"], axis=1)
+    ratio_df[samples] = ratio_df.apply(lambda x: x[samples]/x["pseudo_reference_sample"], axis=1)
 
     norm_df = exon_junction_df.copy()
 

src/main.py
@@ -1,8 +1,7 @@
 # Global imports
 import argparse
-from plotting.vulcano_plot import vulcano_plot
-from plotting.plot_binned_mutations import plot_binned_mutations
-from help_functions.differently_expressed_exons import differently_expressed_linked_exons
+from plotting.plotters import vulcano_plot, plot_binned_mutations, ma_plot, plot_gene_heatmap
+from help_functions.differently_expressed_exons import differently_expressed_linked_exons, differently_expressed_exons
 from help_functions.find_expression import find_expressions
 from help_functions.median_of_ratios_normalization import median_of_ratios_normalization
 from mafWrapper import mafWrapper
@@ -71,6 +70,7 @@ if args.save:
 else:
     save = False
 
+show = False
 
 # Read in features from csv
 
@@ -78,6 +78,7 @@ df = pd.read_csv(args.features[0], sep=args.sep[0])
 
 # Drop ending of tumor type, not needed as we dont have multiple samples from same patients
 df.columns = df.columns.str.replace('-01A', '')
+s = df.columns[12:]
 
 # Drop duplicate rows and remove features with unknown gene location.
 df.drop_duplicates(inplace=True)
@@ -85,7 +86,6 @@ df = df[df["geneName"].str.contains("nan") == False]
 df = df[df[df.columns[12:]].sum(axis=1) > 5]
 
 # Save name of all sample, maybe this should be read from a file
-s = df.columns[12:]
 
 
 # Read maf_path and initatie the wrapper for searching mutations in MAFs
@@ -95,24 +95,28 @@ dfs = mafWrapper(os.path.abspath(args.MAF[0]))
 
 median_ratio_norm = median_of_ratios_normalization(df, s)
 median_ratio_norm = median_ratio_norm[median_ratio_norm["geneName"].notna()]
+median_ratio_norm_centerd = median_ratio_norm.copy()
+median_ratio_norm_centerd[s] = median_ratio_norm_centerd[s].sub(median_ratio_norm_centerd[s].mean(axis=1), axis=1)
 median_sorted = find_expressions(
-    df=median_ratio_norm, dfs=dfs, raw_df=df, col=s, show=False)
+    df=median_ratio_norm_centerd, dfs=dfs, raw_df=df, col=s, show=False)
 
 plot_binned_mutations(median_sorted, "is_related_mutated", "Distribution of samples with mutations, where mutations is close to the exon in question. Left is features with highest expression.",
                       path=outpath, filetype=args.plot_filetype, write=save)
 
 
+differently_exons = differently_expressed_exons(median_ratio_norm, dfs, s)
+
 linked_expression = differently_expressed_linked_exons(median_ratio_norm, dfs)
+
+
+ma_plot(differently_exons, "mean_expression", "fold_change", dfs, filename=os.path.join(outpath, f"ma_single_exons.{args.plot_filetype}"), show=show, save=True)
+
 
 vulcano_plot(linked_expression, x='fold_change', y='p_value', dfs=dfs, save=save,
              filename=os.path.join(outpath, f"linked_expression_vulcano.{args.plot_filetype}", ))
 
 
 if save:
-    median_ratio_norm.to_csv(os.path.join(outpath, "normalized_counts.csv"))
-    median_sorted.to_csv(os.path.join(outpath, "stacked_normalized_counts.csv"))
+    median_ratio_norm.to_csv(os.path.join(outpath, "normalized_counts.csv"), sep=";")
+    median_sorted.to_csv(os.path.join(outpath, "stacked_normalized_counts.csv"), sep=";")
 
-
-
-print(os.path.join(
-    outpath, f"linked_expression_vulcano.{args.plot_filetype}"))

src/plotting/plotters.py
@@ -53,32 +53,30 @@ def ma_plot(df, x, y, dfs, show=False, save=False, filename=None):
 
     plt.scatter(x=np.log2(df[x]), y=df[y],
                 s=1, label="Not significant")
-    #muts = dfs.get_df()
+    muts = dfs.get_df()
     # highlight down- or up- regulated genes
-    #down = df[(df[x] <= -2) & (df[y] <= 0.01)]
-    #up = df[(df[x] >= 2) & (df[y] <= 0.01)]
-    #print("\n\n\n", up.head(), "\n\n\n")
-    #print("\n\n\n", down.head(), "\n\n\n")
-    #plt.scatter(x=down[x], y=down[y].apply(
-    #    lambda x: -np.log10(x)), s=3, label="Down-regulated", color="blue")
-    #plt.scatter(x=up[x], y=up[y].apply(lambda x: -np.log10(x)),
-    #            s=3, label="Up-regulated", color="red")
+    down = df[(df[y] <= -2)]
+    up = df[(df[y] >= 2)]
+    plt.scatter(x=down[x].apply(lambda x: np.log2(x)), y=down[y], s=3, label="Down-regulated", color="blue")
+    plt.scatter(x=up[x].apply(lambda x: np.log2(x)), y=up[y],
+                s=3, label="Up-regulated", color="red")
 
-    #texts = []
-    #for i, r in up.iterrows():
-    #    texts.append(plt.text(x=r[x], y=-np.log10(r[y]), s=muts[muts["Entrez_Gene_Id"]
-    #                 == int(r["geneName"])]["Hugo_Symbol"].unique()[0]))
-    #for i, r in down.iterrows():
-    #    texts.append(plt.text(x=r[x], y=-np.log10(r[y]), s=muts[muts["Entrez_Gene_Id"]
-    #                 == int(r["geneName"])]["Hugo_Symbol"].unique()[0]))
+    texts = []
+    for i, r in up.iterrows():
+        texts.append(plt.text(x=np.log2(r[x]), y=r[y], s=muts[muts["Entrez_Gene_Id"]
+                     == int(r["geneName"])]["Hugo_Symbol"].unique()[0]))
+    for i, r in down.iterrows():
+        texts.append(plt.text(x=np.log2(r[x]), y=r[y], s=muts[muts["Entrez_Gene_Id"]
+                     == int(r["geneName"])]["Hugo_Symbol"].unique()[0]))
 
     # adjust_text(texts,arrowprops=dict(arrowstyle="-", color='black', lw=0.5))
-    plt.xlabel("Mean expression")
+    plt.xlabel("Mean normalized expression")
     plt.ylabel("-logFDR")
-    #plt.axvline(-2, color="grey", linestyle="--")
-    #plt.axvline(2, color="grey", linestyle="--")
-    #plt.axhline(2, color="grey", linestyle="--")
-    #plt.legend()
+    mean_fdr = df[(df[y] != np.inf) & (df[y]  != -np.inf) & (~df[y].isna())][y].mean()
+    plt.axhline(mean_fdr, color="red", linestyle="--",label=f"Mean {mean_fdr}")
+    plt.axhline(2, color="grey", linestyle="--")
+    plt.axhline(-2, color="grey", linestyle="--")
+    plt.legend()
     plt.autoscale()
 
     if save: